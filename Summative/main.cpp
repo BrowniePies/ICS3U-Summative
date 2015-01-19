611b315e4436aa9f4dd0ce7e725caf9684739672
Bradytown <braedonleonard@gmail.com>
2015-01-19 12:50:16 -0500

    Dah best
    Aasdfasdfasdasfasdf

--- Summative/main.cpp
+++ Summative/main.cpp
@@ -6,13 +6,13 @@ using namespace std;
 int main()
 {
     char Name[50];
+    char LOL[111]
 
     cout << "Welcome to our game!" << endl;
     cout << "Please enter your name to continue." << endl;
+    cout << "Kill Yourself"
+    cin >> LOL //Kills self
     cin >> Name //Asks user for their name
     cout << "Hello " << Name << "." << endl;
-    cout << "Time to go on an adventure!" << endl;
-
-    cout << "You enter a dank forest." << endl;
     return 0;
 }
