ccb4dc7d5a2058163980a719181fc3bb21024135
BrowniePies <akhir.games@gmail.com>
2015-01-20 13:52:06 -0500

    Made some story rich

--- Summative/main.cpp
+++ Summative/main.cpp
@@ -17,13 +17,15 @@ int main()
     getch();
     system("CLS");
     cout << "You go on a adventure!" << endl;
-    cout << "On this adventure, you will encounter many obstacles. Goblins, cyborgs, kobolds, and even Cyber-Dragons." <<endl;
+    cout << "On this adventure, you will encounter many obstacles. goblins, cyborgs, kobolds, and even Cyber-Dragons." <<endl;
     cout << "\nAs you can see, you live in a time of Futuristic Technology, but magic and monsters have returned to the lands"<<endl<<endl;
     cout << "Press any key to continue." << endl;
     getch();
     system("CLS");
     cout << "You enter a small town at the outskirts of the great kingdom of INSERT NAME" << endl;
-
+    cout << "This town has been raveged by the goblins and kobolds" << endl;
+    cout << "It seems as though there will be another attack later this day" << endl;
+    cout << "Do you stay and help them defend, or do you leave and carry on?" << endl;
 
 
 
