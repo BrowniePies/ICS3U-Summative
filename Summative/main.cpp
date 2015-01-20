a0899cf69dcf50400d0caaa159b72cdf14cf1d9b
BrowniePies <akhir.games@gmail.com>
2015-01-20 13:23:16 -0500

    PRESS ANY KEY

--- Summative/main.cpp
+++ Summative/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <windows.h>
+#include<conio.h>
 
 using namespace std;
 
@@ -11,6 +12,8 @@ int main()
     cout << "Please enter your name to continue." << endl;
     cin >> Name; //Asks user for their name
     cout << "Hello " << Name << "." << endl;
+    cout << "\nPress any key to continue." << endl;
+    getch();
     system("CLS");
     cout << "You go on a adventure!" << endl;
     return 0;
