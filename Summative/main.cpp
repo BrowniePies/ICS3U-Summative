5f6b7403c7d4dc78735f34ddd8ca25ef9ab1b85d
BrowniePies <akhir.games@gmail.com>
2015-01-20 13:03:54 -0500

    Simple Introdcution Complete

--- Summative/main.cpp
+++ Summative/main.cpp
@@ -6,20 +6,13 @@ using namespace std;
 int main()
 {
     char Name[50];
-    char LOL[111]
 
     cout << "Welcome to our game!" << endl;
     cout << "Please enter your name to continue." << endl;
-<<<<<<< HEAD
     cin >> Name; //Asks user for their name
     cout << "Hello " << Name << "." << endl;
-    cout << "Time to go on an adventure!" << endl;
-    cout << "You enter a dark forest." << endl;
-=======
-    cout << "Kill Yourself"
-    cin >> LOL //Kills self
-    cin >> Name //Asks user for their name
-    cout << "Hello " << Name << "." << endl;
->>>>>>> origin/master
+    Sleep(3000);
+    system("CLS");
+    cout << "You go on a adventure!" << endl;
     return 0;
 }
