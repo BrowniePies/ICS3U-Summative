5fb940790505046dcaaedf16c906e229d63f1a90
BrowniePies <akhir.games@gmail.com>
2015-01-22 13:13:30 -0500

    I read the comments

--- Summative/main.cpp
+++ Summative/main.cpp
@@ -9,14 +9,12 @@ int main()
 {
     string Name;
     string helptown;
+    int karma = 0;
 
+    //Intoduction
     cout << "Welcome to our game!" << endl;
     cout << "Please enter your name to continue." << endl;
-<<<<<<< HEAD
     getline(cin,Name);
-=======
-    getline(cin, Name);
->>>>>>> 236733f4ad3e57f2c394e7b130f733cc299f719c
     cout << "Hello " << Name << "." << endl;
     cout << "\nPress any key to continue." << endl;
     getch();
@@ -27,37 +25,29 @@ int main()
     cout << "Press any key to continue." << endl;
     getch();
     system("CLS");
+
+    //First Choice
     cout << "You enter a small town at the outskirts of the great Kingdom of Hearts" << endl;
     cout << "This town has been raveged by the goblins and kobolds" << endl;
     cout << "It seems as though there will be another attack later this day" << endl;
-<<<<<<< HEAD
     cout << "Do you stay and help them defend, or do you leave and carry on?" << endl;
     getline(cin,helptown);
-    if (helptown == "yes" or helptown == "stay")
-=======
-    cout << "Do you stay and help them defend?" << endl;
-    cin >> helptown;
-    if (helptown == "yes")
->>>>>>> 236733f4ad3e57f2c394e7b130f733cc299f719c
+    if (helptown == "yes" or helptown == "Yes" or helptown == "stay")
     {
-        cout << "YES" << endl;
+        cout << "You stay and wait for the attack to commence" << endl;
         //Make shit happen here and change some values or something
         //Also start working on a combat system
         //It doesn't have to be great, just random numbers and shit
         //Also have our game based around karma, and your karma changes the ending
     }
-    else if (helptown == "no" or helptown == "carry on")
+    else if (helptown == "no" or helptown == "No" or helptown == "carry on")
     {
         cout << "You decide to leave the town for dead." << endl;
 
     }
     else
     {
-<<<<<<< HEAD
         cout << "Your answer was too stange to be taken seriously." << endl;
-=======
-        cout << "Your answer was too strange to be taken seriously."
->>>>>>> 236733f4ad3e57f2c394e7b130f733cc299f719c
     }
     cout << "And so, you move on with your journey." << endl;
 
