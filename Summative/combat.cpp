c40f9a6366cc652bb1af9dd724d5a628e2bc5c63
Bradytown <braedonleonard@gmail.com>
2015-01-23 12:20:29 -0500

    changed stuff

--- Summative/combat.cpp
+++ Summative/combat.cpp
@@ -1,12 +1,14 @@
+#include <iostream>
+#include <string>
+
 using namespace std;
-void combat(string monstername, int monsterhealth)
-{
+
+void combat(string monstername, int monsterhealth){
     string choice;
-    extern int hp;
+
 
     cout << "The fight begins!" << endl;
-    while (monsterhealth >= 0)
-    {
+    while (monsterhealth >= 0){
         cout << "The " << monstername << " has " << monsterhealth << "hp." << endl;
         cout << "What do you do?" << endl;
         cin >> choice;
